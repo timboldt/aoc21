d21b5654b950a10f24318acbcf83c2c687a990f0
Tim Boldt <tim.boldt@gmail.com>
2021-12-25 22:24:11 -0800

    Day 13

--- day13/solution.go
+++ day13/solution.go
@@ -1,19 +1,145 @@
 package day13
 
 import (
+	"bytes"
+	"fmt"
+	"strconv"
 	"strings"
 )
 
-func parseInput(input string) []string {
-	return strings.Split(input, "\n")
+type FoldDirection byte
+
+const (
+	FoldUpward FoldDirection = 'y'
+	FoldLeft   FoldDirection = 'x'
+)
+
+type Dot struct {
+	x int
+	y int
+}
+
+type Fold struct {
+	dir FoldDirection
+	loc int
+}
+
+type Paper struct {
+	dots   []Dot
+	folds  []Fold
+	width  int
+	height int
+}
+
+func parseInput(input string) Paper {
+	var (
+		dots       []Dot
+		folds      []Fold
+		width      int
+		height     int
+		parseFolds bool
+	)
+	for _, line := range strings.Split(input, "\n") {
+		if len(line) == 0 {
+			parseFolds = true
+			continue
+		}
+		if parseFolds {
+			// Parse folds.
+			fold := strings.Split(line, "=")
+			if len(fold) != 2 {
+				panic(fmt.Errorf("unparsable fold: %s", line))
+			}
+			dir := FoldDirection(fold[0][len(fold[0])-1])
+			loc, err := strconv.Atoi(fold[1])
+			if err != nil {
+				panic(err)
+			}
+			folds = append(folds, Fold{dir, loc})
+			if dir == FoldUpward && loc*2+1 > height {
+				height = loc*2 + 1
+			}
+			if dir == FoldLeft && loc*2+1 > width {
+				width = loc*2 + 1
+			}
+		} else {
+			// Parse dots.
+			xy := strings.Split(line, ",")
+			if len(xy) != 2 {
+				panic(fmt.Errorf("unparsable dot: %s", line))
+			}
+			x, err := strconv.Atoi(xy[0])
+			if err != nil {
+				panic(err)
+			}
+			y, err := strconv.Atoi(xy[1])
+			if err != nil {
+				panic(err)
+			}
+			dots = append(dots, Dot{x, y})
+		}
+	}
+	return Paper{dots: dots, folds: folds, width: width, height: height}
+}
+
+func applyFold(paper *Paper) {
+	fold := paper.folds[0]
+	paper.folds = paper.folds[1:]
+	if fold.dir == FoldUpward {
+		for i := range paper.dots {
+			if paper.dots[i].y > fold.loc {
+				paper.dots[i].y = 2*fold.loc - paper.dots[i].y
+			}
+		}
+		paper.height = fold.loc
+	} else {
+		for i := range paper.dots {
+			if paper.dots[i].x > fold.loc {
+				paper.dots[i].x = 2*fold.loc - paper.dots[i].x
+			}
+		}
+		paper.width = fold.loc
+	}
+}
+
+func countUniqueDots(dots []Dot) int {
+	m := make(map[Dot]bool)
+	for _, d := range dots {
+		m[d] = true
+	}
+	return len(m)
+}
+
+func (p *Paper) String() string {
+	var b bytes.Buffer
+	m := make(map[Dot]bool)
+	for _, d := range p.dots {
+		m[d] = true
+	}
+	for row := 0; row < p.height; row++ {
+		for col := 0; col < p.width; col++ {
+			if m[Dot{x: col, y: row}] {
+				b.WriteRune('#')
+			} else {
+				b.WriteRune('.')
+			}
+		}
+		b.WriteRune('\n')
+	}
+	return b.String()
 }
 
 func Part1(input string) int {
-	lines := parseInput(input)
-	return len(lines)
+	paper := parseInput(input)
+	applyFold(&paper)
+	return countUniqueDots(paper.dots)
 }
 
 func Part2(input string) int {
-	lines := parseInput(input)
-	return len(lines)
+	paper := parseInput(input)
+	for len(paper.folds) > 0 {
+		applyFold(&paper)
+	}
+	fmt.Println(paper.String())
+	return 0
 }
--- day13/solution_test.go
+++ day13/solution_test.go
@@ -6,20 +6,40 @@ import (
 )
 
 var sampleText = strings.Trim(`
-XXX`, "\n")
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`, "\n")
 
 func TestPart1(t *testing.T) {
-	want := 999
+	want := 17
 	got := Part1(sampleText)
 	if got != want {
 		t.Fatalf(`Got %d, want %d`, got, want)
 	}
 }
 
-func TestPart2(t *testing.T) {
-	want := 999
-	got := Part2(sampleText)
-	if got != want {
-		t.Fatalf(`Got %d, want %d`, got, want)
-	}
-}
+// func TestPart2(t *testing.T) {
+// 	want := 999
+// 	got := Part2(sampleText)
+// 	if got != want {
+// 		t.Fatalf(`Got %d, want %d`, got, want)
+// 	}
+// }
